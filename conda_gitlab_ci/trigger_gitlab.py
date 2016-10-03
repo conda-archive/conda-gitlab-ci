3a5110c23112450765f11144968ab27053adab0b
Michael Sarahan <msarahan@gmail.com>
2016-10-03 15:33:17 -0500

    add gitlab API for getting status.  add tests.

--- conda_gitlab_ci/trigger_gitlab.py
+++ conda_gitlab_ci/trigger_gitlab.py
@@ -1,41 +1,119 @@
-import os
 from itertools import izip, product
+import os
 import urlparse
 
 import requests
 import yaml
+from conda_build import api
 
 
-def get_target_labels(platforms_dir):
+def _get_target_labels(platforms_dir):
     labels = []
     for f in os.listdir(platforms_dir):
-        with open(f) as buff:
+        with open(os.path.join(platforms_dir, f)) as buff:
             platform = yaml.load(buff)
             labels.append(platform['worker_label'])
     return labels
 
 
-def get_versions_product(versions_file):
+def _filter_environment_with_metadata(build_recipe, version_dicts):
+    def del_key(version_dicts, key):
+        if key == 'python':
+            key = 'py'
+        elif key == 'numpy':
+            key = 'npy'
+        elif key == 'r-base':
+            key = 'r'
+        del version_dicts['CONDA_' + key.upper()]
+        return version_dicts
+
+    metadata, _, _ = api.render(build_recipe)
+
+    for name in ('numpy', 'python', 'perl', 'lua', 'r-base'):
+        for req in metadata.get_value('requirements/run'):
+            if hasattr(req, 'encode'):
+                req = req.encode('utf-8')
+            req_parts = req.split(' ')
+            if req_parts[0] == name:
+                # logic here: if a version is provided, then ignore the build matrix - except
+                #   numpy.  If numpy has x.x, that is the only way that it is considered part
+                #   of the build matrix.
+                #
+                # Break = keep the recipe (since we don't fall through to del_key for this name)
+                if len(req_parts) > 1:
+                    if name == 'numpy' and 'x.x' in req_parts:
+                        break
+                    # we have a version specified for something other than numpy.  This means
+                    #    we are overriding our build matrix.  Do not consider this variable.
+                    else:
+                        continue
+                # fall through for numpy when it does not have any associated x.x
+                if name == 'numpy':
+                    continue
+                break
+        else:
+            version_dicts = del_key(version_dicts, name)
+    return version_dicts
+
+
+def _get_versions_product(build_recipe, versions_file):
     with open(versions_file) as f:
         dicts = yaml.load(f)
+    dicts = _filter_environment_with_metadata(build_recipe, dicts)
     # http://stackoverflow.com/a/5228294/1170370
     return (dict(izip(dicts, x)) for x in product(*dicts.itervalues()))
 
 
-def submit_builds(build_recipe, dependent_recipes, repo_ref, repo_base_dir,
-                  ci_submit_url=None, ci_submit_token=None):
+def _get_url_from_env_vars(url_type, commit_sha=None):
+    ci_urls = {"trigger": "/api/v3/projects/{id}/trigger/builds",
+               "status": "/api/v3/projects/{id}/repository/commits/{sha}/statuses"}
+    # These CI variables are set by gitlab during a build.
+    base_url = os.getenv("CI_PROJECT_URL")
+    if not base_url:
+        raise ValueError("Did not get value for CI_PROJECT_URL.  Please set this"
+                         "variable and try again.")
+    url = urlparse.urlsplit(base_url)
+    project_id = os.getenv("CI_PROJECT_ID")
+    if not commit_sha:
+        commit_sha = os.getenv("CI_BUILD_REF")
+    if not project_id:
+        raise ValueError("Did not get value for CI_PROJECT_ID.  "
+                            "You must provide ci_submit_url arg if not "
+                            "running under a gitlab ci build.")
+    if not commit_sha:
+        raise ValueError("Did not get value for CI_BUILD_REF.  "
+                            "You must provide ci_submit_url arg if not "
+                            "running under a gitlab ci build.")
+    location = ci_urls[url_type].format(id=project_id, sha=commit_sha)
+    ci_url = urlparse.urlunsplit((url.scheme, url.hostname, location,
+                                  "", ""))
+    return ci_url
+
+
+def expand_build_matrix(build_recipe, repo_base_dir, repo_ref=None):
+    platforms_dir = os.path.join(repo_base_dir, 'platforms.d')
+    labels = _get_target_labels(platforms_dir)
+    configurations = []
+    if not os.path.isabs(build_recipe):
+        build_recipe = os.path.join(repo_base_dir, build_recipe)
+    for label in labels:
+        version_sets = _get_versions_product(build_recipe,
+                                             os.path.join(repo_base_dir,
+                                                          'versions.yml'))
+        for version_set in version_sets:
+            version_set.update({
+                    "TARGET_PLATFORM": label,
+                    "BUILD_RECIPE": build_recipe,
+            })
+            configurations.append({'variables': version_set})
+    return configurations
+
+
+def submit_build(configuration, repo_ref, ci_submit_url=None, ci_submit_token=None):
+    """returns job id for later checking on status"""
     if not ci_submit_url:
-        # These CI variables are set by gitlab.  We allow passing the URL more
-        #    directly for the sake of testing.
-        url = urlparse.urlsplit(os.getenv("CI_PROJECT_URL"))
-        project_id = os.getenv("CI_PROJECT_ID")
-        if not project_id:
-            raise ValueError("Did not get value for CI_PROJECT_ID.  "
-                             "You must provide ci_submit_url arg if not "
-                             "running under a gitlab ci build.")
-        location = "/api/v3/projects/{0}/trigger/builds".format(project_id)
-        ci_submit_url = urlparse.urlsplit((url.scheme, url.hostname, location,
-                                           "", ""))
+        ci_submit_url = _get_url_from_env_vars('trigger')
+
     if not ci_submit_token:
         ci_submit_token = os.getenv('TRIGGER_TOKEN')
         if not ci_submit_token:
@@ -44,18 +122,36 @@ def submit_builds(build_recipe, dependent_recipes, repo_ref, repo_base_dir,
                              "running under a gitlab ci build.  Also, you must"
                              "set the TRIGGER_TOKEN secret environment "
                              "variable for your project.")
+    configuration.update({
+        'token': ci_submit_token,
+        'ref': repo_ref,
+    })
 
-    platforms_dir = os.path.join(repo_base_dir, 'platforms.d')
-    labels = get_target_labels(platforms_dir)
-    for label in labels:
-        for version_set in get_versions_product(os.path.join(repo_base_dir,
-                                                             'versions.yml')):
-            requests.post(ci_submit_url, data={
-                'variables': version_set.update({
-                    "TARGET_PLATFORM": label,
-                    "BUILD_RECIPE": build_recipe,
-                    "DEPENDENT_RECIPES": dependent_recipes,
-                }),
-                'token': ci_submit_token,
-                'ref': repo_ref,
-            })
+    response = requests.post(ci_submit_url, json=configuration)
+    return response.json()['id']
+
+
+def check_build_status(build_id, repo_ref=None, ci_status_url=None):
+    """
+    Queries status of build.  Note that build_id and repo_ref are strongly tied.
+       If a build_id does not exist for a given repo_ref, then you'll get an
+       empty list back.
+
+    returns one of:
+      - success
+      - pending
+      - running
+      - failed
+    """
+    if not ci_status_url:
+        ci_status_url = _get_url_from_env_vars('status', repo_ref)
+    # need a token to use API.  This should be set using private variables.
+    private_token = os.getenv("GITLAB_PRIVATE_TOKEN")
+    if not private_token:
+        raise ValueError("Did not get value for GITLAB_PRIVATE_TOKEN.  "
+                        "You must set the GITLAB_PRIVATE_TOKEN secret environment "
+                        "variable for your project.")
+    ci_status_url = urlparse.urljoin(ci_status_url, '?private_token=' + private_token)
+    response = requests.get(ci_status_url).json()
+    status = [build['status'] for build in response if int(build['id']) == build_id][0]
+    return status
---
+++ tests/test_trigger_gitlab.py
@@ -0,0 +1,62 @@
+import os
+import responses
+
+from conda_gitlab_ci import trigger_gitlab
+import pytest
+
+test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+
+@pytest.fixture
+def set_ci_environ_vars():
+    os.environ['GITLAB_PRIVATE_TOKEN'] = 'private_token_value'
+    os.environ['TRIGGER_TOKEN'] = 'trigger_token_value'
+    os.environ['CI_BUILD_REF'] = "123abc"
+    os.environ['CI_PROJECT_ID'] = "2"
+    os.environ['CI_PROJECT_URL'] = "http://some.test.ci.com/somegroup/projectname"
+
+
+@responses.activate
+def test_check_build_status(set_ci_environ_vars):
+    responses.add(responses.GET,
+                  'http://some.test.ci.com/api/v3/projects/2/repository/commits/123abc/statuses',
+                  status=200,
+                  json=[{"id": 1, "status": "success"}, {"id": 2, "status": "failed"}],
+                  content_type='application/json')
+    assert trigger_gitlab.check_build_status(1, repo_ref='123abc') == 'success'
+    assert trigger_gitlab.check_build_status(2, repo_ref='123abc') == 'failed'
+
+
+@responses.activate
+def test_submit_build(set_ci_environ_vars):
+    responses.add(responses.POST,
+                  'http://some.test.ci.com/api/v3/projects/2/trigger/builds',
+                  status=201,
+                  json={'id': 1, 'variables': {'BUILD_RECIPE': 'frank'}},
+    )
+    vars = {"variables": {"BUILD_RECIPE": "frank"}}
+    assert trigger_gitlab.submit_build(vars, '123abc') == 1
+
+
+def test_expand_build_matrix():
+    # python version specified; ignores matrix for python
+    # (win + mac + linux) = 3
+    configurations = trigger_gitlab.expand_build_matrix('python_version_specified',
+                                                        repo_base_dir=test_data_dir)
+    assert len(configurations) == 3
+
+    # python version not specified; uses matrix for python
+    # (python 2 + python 3) * (win + mac + linux) = 6
+    configurations = trigger_gitlab.expand_build_matrix('python_test',
+                                                        repo_base_dir=test_data_dir)
+    assert len(configurations) == 6
+
+    # (python 2 + python 3) * (win + mac + linux) = 6
+    configurations = trigger_gitlab.expand_build_matrix('python_numpy_no_xx',
+                                                        repo_base_dir=test_data_dir)
+    assert len(configurations) == 6
+
+    # (python 2 + python 3) * (numpy 1.10 + 1.11) * (win + mac + linux) = 12
+    configurations = trigger_gitlab.expand_build_matrix('python_numpy_xx',
+                                                        repo_base_dir=test_data_dir)
+    assert len(configurations) == 12
